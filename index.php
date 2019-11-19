dee5600719a80b364a191b42036ba6eea8c4a05c
Francesco <cescolofaro@gmail.com>
2019-11-19 18:02:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 function fw() {
-  echo "test";
+  $curl = curl_init();
+  $cod = $_GET["codice"];
+  $txt = $_GET["testo"];
+  
+  curl_setopt_array($curl, array(
+        	CURLOPT_URL => "https://francescolf.altervista.org/notifiche?codice=" . $cod + "&testo=" . $txt,
+        	CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "GET",
+          CURLOPT_HTTPHEADER => array(
+                    "content-type: application/json"
+                ),
+            ));
+
+        $response9 = curl_exec($curl);
 }
 fw();
 ?>
