0ec86d60123b4558bb778462941c01d36e43f7a6
Francesco <cescolofaro@gmail.com>
2019-11-19 19:41:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function getUrlParameter(name) {
+function get(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function fw() {
+    var cod = get("codice");
+    alert(cod);
 }
